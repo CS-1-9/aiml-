ee71f56a070d1e0c62953ff19631291a37cc24d2
K Chandan Shenoy <chandanshenoy91@gmail.com>
2025-12-08 21:32:33 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,8 @@
+# file creating "
+file = open("demo1.txt","w")
+file.write("this is file handling in python \n")
+file.write("this is Chandan \n")
+file.write("we are learning file handling \n")
+print("file created")
+
+file.close()
